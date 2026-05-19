76b7121d2d0de73da9e1e41d6710161d6ba0a9b8
Mijura <miodragvilotijevic@gmail.com>
2026-05-19 21:25:15 +0200

    streaming function call

---
+++ streaming-function-call.js
@@ -0,0 +1,32 @@
+import { OpenAI } from "openai";
+
+const openai = new OpenAI();
+import "dotenv/config"
+
+const tools = [{
+    type: "function",
+    name: "get_weather",
+    description: "Get current temperature for provided coordinates in celsius.",
+    parameters: {
+        type: "object",
+        properties: {
+            latitude: { type: "number" },
+            longitude: { type: "number" }
+        },
+        required: ["latitude", "longitude"],
+        additionalProperties: false
+    },
+    strict: true
+}];
+
+const stream = await openai.responses.create({
+    model: "gpt-4.1",
+    input: [{ role: "user", content: "What's the weather like in Paris today?" }],
+    tools,
+    stream: true,
+    store: true,
+});
+
+for await (const event of stream) {
+    console.log(event)
+}
