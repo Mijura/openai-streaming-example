d08dc749b0b32a291bb384d6ea5ddb304df263ac
Mijura <miodragvilotijevic@gmail.com>
2026-05-19 21:31:19 +0200

    streaming structured output

---
+++ streaming-structured-output.js
@@ -0,0 +1,25 @@
+import { OpenAI } from "openai";
+import { zodTextFormat } from "openai/helpers/zod";
+import { z } from "zod";
+import "dotenv/config"
+
+const EntitiesSchema = z.object({
+attributes: z.array(z.string()),
+colors: z.array(z.string()),
+animals: z.array(z.string()),
+});
+
+const openai = new OpenAI();
+const stream = await openai.responses.stream({
+    model: "gpt-4.1",
+    input: [
+        { role: "user", content: "What's the weather like in Paris today?" },
+    ],
+    text: {
+        format: zodTextFormat(EntitiesSchema, "entities"),
+    },
+});
+
+for await (const event of stream) {
+    console.log(event);
+}
